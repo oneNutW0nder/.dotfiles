d70a56f0c51b0a98122cfeb143867e4a0b018977
Simon B <scb5436@rit.edu>
2022-05-30 01:04:55 -0400

    add todo

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,10 @@
 --- Here goes a lot of time...
 ----------------------------------------------
 
+-- TODO:
+--  Breakout settings and maps into separate files again... this is too messy
+--  See this format of breaking out: https://github.com/icyphox/dotfiles/tree/master/config/nvim
+
 --
 -- Begin External lua
 --
