173616d9cb0bef7ed3a702a1000cb3d51f64f32f
Simon B <scb5436@rit.edu>
2022-06-09 01:01:53 -0400

    telescope binds and new mapper func

--- nvim/.config/nvim/lua/onenutw0nder/keybinds.lua
+++ nvim/.config/nvim/lua/onenutw0nder/keybinds.lua
@@ -6,27 +6,40 @@
 vim.g.mapleader = ' '
 
 -- shorthand for setting keymaps
-local mapper = vim.api.nvim_set_keymap
+local mapper = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
-mapper("", "<leader>h", ":nohlsearch<cr>", { silent = true } )
+mapper("", "<leader>h", ":nohlsearch<cr>", opts )
 
 -- Better window navigation
-mapper("n", "<C-l>", "<C-w>l", { noremap = true, silent = true } )
-mapper("n", "<C-h>", "<C-w>h", { noremap = true, silent = true } )
-mapper("n", "<C-k>", "<C-w>k", { noremap = true, silent = true } )
-mapper("n", "<C-j>", "<C-w>j", { noremap = true, silent = true } )
+mapper("n", "<C-l>", "<C-w>l", opts )
+mapper("n", "<C-h>", "<C-w>h", opts )
+mapper("n", "<C-k>", "<C-w>k", opts )
+mapper("n", "<C-j>", "<C-w>j", opts )
 
 -- Reselect visual selection after indenting
-mapper("v", "<", "<gv", { noremap = true, silent = true } )
-mapper("v", ">", ">gv", { noremap = true, silent = true } )
+mapper("v", "<", "<gv", opts )
+mapper("v", ">", ">gv", opts )
 
 -- Make 'Y' behave like other capitol letters
-mapper("", "Y", "y$", { noremap = true, silent = true } )
+mapper("", "Y", "y$", opts )
 
 -- Allow gf to open non-existent files
-mapper("", "gf", ":edit <cfile><cr>", { silent = true } )
+mapper("", "gf", ":edit <cfile><cr>", opts )
 
 -- search matches in the middle of the window
-mapper('n', 'n', 'nzzzv', { silent = true } )
-mapper('n', 'N', 'Nzzzv', { silent = true } )
+mapper('n', 'n', 'nzzzv', opts )
+mapper('n', 'N', 'Nzzzv', opts )
 
+-- Telescope binds
+local telescope = require("telescope.builtin")
+
+-- Grep for a string globally and allow filtering of results in telescope
+vim.api.nvim_set_keymap("n", "<leader>ss", ":lua require('telescope.builtin').grep_string({search = vim.fn.input('Grep for > ')})<cr>", opts )
+
+-- Grep for string under the cursor and filter with telescope
+vim.api.nvim_set_keymap("n", "<leader>sw", ":lua require('telescope.builtin').grep_string({search = vim.fn.expand('<cword>')})<cr>", opts )
+
+mapper("n", "<leader>sf", telescope.find_files, opts )
+mapper("n", "<leader>sb", telescope.buffers, opts )
+mapper("n", "<leader>sg", telescope.git_files, opts )
