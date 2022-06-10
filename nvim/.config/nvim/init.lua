fc9b28b074b3e6db50baee1e913e1d0b31448516
Simon B <scb5436@rit.edu>
2022-06-10 14:47:59 -0400

    todo

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,8 +2,9 @@
 ----------------------------------------------
 
 -- TODO:
+  -- Make snippets useful and the keybinds friendly
+  -- Make jumping to definitions and things work with and without telescope (if there is only 1 place to go; don't open telesocpe)
   -- Make all of the lua files more organized with some kind of folder structure
-  -- Add windwp/nvim-autopairs for better experience
   -- Get treesitter figured out and configured for actual use
   -- Get telescope binds figured out for actual use
   -- Get git integration and icons in the sign column (maybe something like LazyGit)
