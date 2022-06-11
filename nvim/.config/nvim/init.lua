bb85ea1c7830903fada799b1a02a8f902f0209e1
Simon B <scb5436@rit.edu>
2022-06-11 13:43:06 -0400

    comments about project structure

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,6 +8,12 @@
   -- Get treesitter figured out and configured for actual use
   -- Get telescope binds figured out for actual use
   -- Get git integration and icons in the sign column (maybe something like LazyGit)
+  --
+  -- New project layout that will improve organization and hopefully load time!
+    -- init.lua --> should only require 'config' and 'plugins' (maybe a 'utils')
+      -- Everything will be configured in 'plugins' and settings contained in respective files in 'lua/config/'
+    -- Layout will be similar to: https://github.com/alpha2phi/neovim-for-beginner
+
 
 --
 -- Begin External lua
