04e20d5e18c69b12eb0c78566b34523478de85fc
Simon B <scb5436@rit.edu>
2022-06-20 10:47:31 -0400

    colors

--- nvim/.config/nvim/lua/config/settings.lua
+++ nvim/.config/nvim/lua/config/settings.lua
@@ -68,12 +68,13 @@ set_cursorline("WinEnter", true)
 set_cursorline("FileType", false, "TelescopePrompt")
 
 -- color scheme - vim.cmd settings are from 'ThePrimeagen'
-g.gruvbox_contrast_dark = "soft"
-g.gruvbox_italic = 1
-g.gruvbox_italicize_strings = 0
-g.gruvbox_invert_selection = 0
-g.gruvbox_invert_signs = 0
-vim.cmd [[ colorscheme gruvbox ]]
+-- g.gruvbox_contrast_dark = "hard"
+-- g.gruvbox_italic = 1
+-- g.gruvbox_italicize_strings = 0
+-- g.gruvbox_invert_selection = 0
+-- g.gruvbox_invert_signs = 0
+-- vim.cmd [[ colorscheme gruvbox ]]
+
 -- vim.cmd [[ let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum" ]]
 -- vim.cmd [[ let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum" ]]
 -- vim.cmd [[ hi CursorLineNR guibg=None ]]
@@ -98,7 +99,7 @@ vim.cmd("colorscheme gruvbox-baby") ]]
 
 -- Status line stuff
 opt.laststatus = 3
-require("lualine").setup({ options = { theme = "gruvbox" } })
+require("lualine").setup({ options = { theme = "gruvbox-material" } })
 
 -- Highlight on yank
 vim.cmd([[
